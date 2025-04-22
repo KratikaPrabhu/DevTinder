58a08c5d89fb7a39012eee644be2fd2be5f4483a
KratikaPrabhu <prabhukrati62@gmail.com>
2025-04-22 18:27:09 +0530

    added app.js in server

--- src/app.js
+++ src/app.js
@@ -4,7 +4,18 @@ const app = express();
 app.use("/test",(req,res)=>{
     res.send("test the cad WORLD");
 })
-
+app.use("/hello/2/6788",(req,res)=>{
+    res.send("hello 6788 the cad WORLD");
+})
+app.use("/hello/2",(req,res)=>{
+    res.send("hello 2222 the cad WORLD");
+})
+app.use("/hello",(req,res)=>{
+    res.send("hello the cad WORLD");
+})
+app.use("/",(req,res)=>{
+    res.send("///// the cad WORLD");
+})
 app.listen(7777,()=>{
     console.log("server is running");
     
